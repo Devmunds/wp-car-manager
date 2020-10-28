67e9ddccebc9fe66ab4f8d9ac99994b220409cda
Devmunds <devmunds@gmail.com>
2020-10-27 22:05:34 -0300

    Update: data-prince

--- templates/vehicle-data/data-price.php
+++ templates/vehicle-data/data-price.php
@@ -6,8 +6,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <!--
 <tr>
-	<th><?php _e( 'Price', 'wp-car-manager' ); ?></th>
-	<td><span class="wpcm-vehicle-data"><?php echo $vehicle->get_formatted_price(); ?></span>
+	<th><?php //_e( 'Price', 'wp-car-manager' ); ?></th>
+	<td><span class="wpcm-vehicle-data"><?php //echo $vehicle->get_formatted_price(); ?></span>
 	</td>
 </tr>
 -->
