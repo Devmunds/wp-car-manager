bc1c55f74adf8c045afbf30b4986b1e44ef53bf2
Devmunds <devmunds@gmail.com>
2021-01-14 18:02:30 -0300

    Update: layout grid vehicles

--- templates/listings/item.php
+++ templates/listings/item.php
@@ -10,13 +10,19 @@
             </div>
             <div class="wpcm-listings-item-description">
                 <?php do_action( 'wpcm_vehicle_listings_item_description_start', $vehicle ); ?>
-                <h3><?php echo  $vehicle->get_title();?></h3>
+                <h2><?php echo  $vehicle->get_make_name() ; ?></h2>
+                <h3><?php  echo $vehicle->get_engine() .  " " . $vehicle->get_fuel_type() . " " . $vehicle->get_formatted_transmission(); ?></h3>
                 <?php do_action( 'wpcm_vehicle_listings_item_description_end', $vehicle ); ?>
             </div>
             <div class="wpcm-listings-item-meta">
-                <?php do_action( 'wpcm_vehicle_listings_item_meta_start', $vehicle ); ?>              
-                    <label><?php echo $vehicle->get_anomod() . " - " . $vehicle->get_color() . " - " . $vehicle->get_fuel_type();?></label><br/>
-                    <span><?php echo "R$ " . number_format($vehicle->get_price(), 2, ',', '.') ; ?></span>         
+                <?php do_action( 'wpcm_vehicle_listings_item_meta_start', $vehicle ); ?>
+                    <div class="wpcm-item-price">
+                        <span><?php echo "R$ " . number_format($vehicle->get_price(), 2, ',', '.') ; ?></span>       
+                    </div>
+                    <div class="wpcm-item-ano-model-fab">
+                        <span><?php echo $vehicle->get_anomod() . "/" . $vehicle->get_anofab()?></span>
+                        <span class="pull-right"><?php echo $vehicle->get_mileage(); ?> Km</span>
+                    </div>
                 <?php do_action( 'wpcm_vehicle_listings_item_meta_end', $vehicle ); ?>
             </div>
             <?php do_action( 'wpcm_vehicle_listings_item_end', $vehicle ); ?>
